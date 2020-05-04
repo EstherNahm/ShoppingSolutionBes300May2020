ffede81628f5dd2b08757c913fd574ab11999993
Jeffry Gonzalez <jeff@hypertheory.com>
2020-05-04 12:50:38 -0400

    Add project files.

---
+++ ShoppingApi/Controllers/ShoppingListController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ShoppingApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShoppingApi.Controllers
+{
+    public class ShoppingListController : ControllerBase
+    {
+        
+        [HttpGet("shoppinglist")]
+        public async Task<ActionResult> GetFullShoppingList()
+        {
+            // Step 1
+            var response = new GetShoppingListResponse();
+            // Step 2??
+
+            // Step 3 PROFIT!
+            return Ok(response);
+        }
+    }
+}
