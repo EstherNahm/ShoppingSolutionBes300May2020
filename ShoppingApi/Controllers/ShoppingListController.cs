101c9f4db10135f9532f3d7edad7b5f9cccd5324
Jeffry Gonzalez <jeff@hypertheory.com>
2020-05-04 16:06:35 -0400

    done with day 1

--- ShoppingApi/Controllers/ShoppingListController.cs
+++ ShoppingApi/Controllers/ShoppingListController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ShoppingApi.Data;
 using ShoppingApi.Models;
 using System;
 using System.Collections.Generic;
@@ -9,14 +11,26 @@ namespace ShoppingApi.Controllers
 {
     public class ShoppingListController : ControllerBase
     {
-        
+        private readonly ShoppingDataContext DataContext;
+
+        public ShoppingListController(ShoppingDataContext dataContext)
+        {
+            DataContext = dataContext;
+        }
+
         [HttpGet("shoppinglist")]
         public async Task<ActionResult> GetFullShoppingList()
         {
             // Step 1
             var response = new GetShoppingListResponse();
             // Step 2??
-
+            response.Data = await DataContext.ShoppingItems
+                .Select(item => new ShoppingListItemResponse
+                {
+                    Id = item.Id,
+                    Description = item.Description,
+                    Purchased = item.Purchased
+                }).ToListAsync();
             // Step 3 PROFIT!
             return Ok(response);
         }
